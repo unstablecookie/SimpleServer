7725af1faa630d9dbabbfdd0bd63d3d9d864aceb
Aleksey Melnikov <realxellor@gmail.com>
2023-08-30 09:41:16 +0300

    fully functional

--- src/Receiver.java
+++ src/Receiver.java
@@ -4,6 +4,7 @@ import java.io.OutputStream;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.net.InetSocketAddress;
+import java.nio.file.StandardCopyOption;
 import java.net.HttpURLConnection;
 
 import com.sun.net.httpserver.HttpExchange;
@@ -20,6 +21,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.File;
 import java.io.BufferedInputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Arrays;
 
 public class Receiver {
 
@@ -52,15 +57,10 @@ public class Receiver {
 			if(httpexchange.getRequestMethod().equalsIgnoreCase("POST")) {
 				try {
 					Headers requestHeaders = httpexchange.getRequestHeaders();
-					//int contentLength = Integer.parseInt(requestHeaders.getFirst("Content-length"));
-					//System.out.println("Content-length:"+requestHeaders.getFirst("Content-length"));
 					
 					InputStream in = httpexchange.getRequestBody();
 					Scanner s = new Scanner(in).useDelimiter("\\A");
 					String result = s.hasNext() ? s.next() : "" ;			
-					//byte[] bufferData = new byte[contentLength];
-					//int length = in.read(bufferData);
-					//for(byte b: bufferData) System.out.println(b);
 					in.close();
 					System.out.println(result);
 					//response
@@ -82,16 +82,66 @@ public class Receiver {
 	static class MyFileHandler implements HttpHandler{
 		@Override
 		public void handle(HttpExchange httpexchange) throws IOException {
-			Headers requestHeaders = httpexchange.getRequestHeaders();
-			File file = new File("/168.jpg");
-			try(InputStream in  = httpexchange.getRequestBody()){
+			Map<String,List<String>> map = httpexchange.getRequestHeaders();
+			Set<Map.Entry<String,List<String>>> set = map.entrySet();
+			for(Map.Entry entry : set) {
+				System.out.println(entry.getKey()+" : "+ entry.getValue());
+			}
+			//get real length
+			List<String> listrl = map.get("Real-length");
+			List<String> listcl = map.get("Content-length");
+			List<String> listbd = map.get("Content-type");
+			long realLength = Long.parseLong(listrl.get(0));
+			long contentLength = Long.parseLong(listcl.get(0));
+			String[] boundaryParser = listbd.get(0).split(" ");
+			String boundary = boundaryParser[1].substring(9,boundaryParser[1].length());
+			System.out.println("realLength:"+realLength);
+			System.out.println("contentLength:"+contentLength);
+			System.out.println(" boundary parser :" + boundary + " length:"+boundary.length());
+			File file = new File("168.jpg");
+			try(InputStream in  = httpexchange.getRequestBody();
+					FileOutputStream out = new FileOutputStream(file)){
+				for(int i=0;i<contentLength-(realLength+boundary.length()+8);i++)  in.read();//skip header
+				byte[] buffer = new byte[1024];
+				int bytesRead;
+				long end = contentLength-(boundary.length()+8);
+				//long offset = contentLength-(boundary.length()+8);
+				long counter = 0;
+				System.out.println("end:"+ end);
+				
+				outerloop:
+				while((bytesRead = in.read(buffer))!= -1 )//&& bytesRead<=end
+				{
+					counter += bytesRead;
+					if(counter>=realLength) {
+						int offset = (int)(counter-realLength);
+						byte[] finalBuffer = Arrays.copyOfRange(buffer, 0, (bytesRead-offset));//copy partial buffer
+						out.write(finalBuffer,0,(bytesRead-offset));
+						break outerloop;
+					}
+					out.write(buffer,0,bytesRead);
+				}
+				//java.nio.file.Files.copy(in, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+				System.out.println(" file copied");
+			}
+			
+			//responce
+			String responseString = "POST OK";
+			Headers responseHeaders = httpexchange.getResponseHeaders();
+			httpexchange.sendResponseHeaders(200,responseString.length());
+			OutputStream out = httpexchange.getResponseBody();
+			out.write(responseString.length());
+			out.flush();
+			out.close();
+			
+			/*try(InputStream in  = httpexchange.getRequestBody()){
 				FileOutputStream fout = new FileOutputStream(file,false);
 				BufferedInputStream bin = new BufferedInputStream(in);
 				int read;
 				while((read = bin.read())!=-1){
 					fout.write(read);
 				}
-			}
+			}*/
 		}
 	}
 	
